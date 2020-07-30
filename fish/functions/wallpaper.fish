6dc31d72584aef3cb454f905d099898f4eafcba5
Sam Hosseini <git-email@hosseini.io>
2020-07-30 15:30:12 +0300

    update wallpaper function to move image to Uploads folder

--- fish/functions/wallpaper.fish
+++ fish/functions/wallpaper.fish
@@ -1,6 +1,10 @@
 function wallpaper
-    set --local DROPBOX_IMAGE ~/Dropbox/Uploads/todays_picture_*.jpg
+    set --local DOWNLOADED_IMAGE ~/Downloads/*.jpg
+    set --local DROPBOX_IMAGE  ~/Dropbox/Uploads/todays_picture_(random).jpg
+    set --local DROPBOX_IMAGES ~/Dropbox/Uploads/todays_picture_*.jpg
 
+    rm $DROPBOX_IMAGES
+    mv $DOWNLOADED_IMAGE $DROPBOX_IMAGE
     osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$DROPBOX_IMAGE\""
 end
 
