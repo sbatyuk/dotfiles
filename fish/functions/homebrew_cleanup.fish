9bdfed2bbdfcab3f0f4d5c8c16d5f57ce661d894
Sam Hosseini <git-email@hosseini.io>
2020-12-20 20:05:43 +0200

    simplify homebrew_cleanup fish function

--- fish/functions/homebrew_cleanup.fish
+++ fish/functions/homebrew_cleanup.fish
@@ -1,12 +1,8 @@
-function bbc
+function homebrew_cleanup
     set --local TAP  $DOTFILES_REPO/homebrew/tap
     set --local BREW $DOTFILES_REPO/homebrew/brew
     set --local CASK $DOTFILES_REPO/homebrew/cask
     set --local MAS  $DOTFILES_REPO/homebrew/mas
 
-    set --local ALL_PACKAGES $DOTFILES_REPO/homebrew/all_packages
-
-    cat $TAP $BREW $CASK $MAS > $ALL_PACKAGES
-    brew bundle cleanup --file=$ALL_PACKAGES --force 2>&1 | ack --invert-match 'For the cask, use'
-    rm $ALL_PACKAGES
+    cat $TAP $BREW $CASK $MAS | brew bundle cleanup --force --file=-
 end
