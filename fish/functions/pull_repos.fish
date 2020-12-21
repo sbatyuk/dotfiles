6c96117ff41c1bbf2869a14d8ce4318f9ae5339b
Sam Hosseini <git-email@hosseini.io>
2020-12-21 22:14:50 +0200

    update pull_repos function with new find (fd) options

--- fish/functions/pull_repos.fish
+++ fish/functions/pull_repos.fish
@@ -1,5 +1,5 @@
 function pull_repos
-    set --local REPOS (find ~/repos/ -type d -maxdepth 1 -mindepth 1)
+    set --local REPOS (find --type directory --maxdepth 1 . ~/repos/)
 
     parallel --verbose --linebuffer -j (count $REPOS) git -C {} pull ::: $REPOS
 end
