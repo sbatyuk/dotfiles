9b20ce08f83b9177719b9d72c2c853bda89e2620
Sam Hosseini <git-email@hosseini.io>
2020-12-22 01:31:12 +0200

    refactor unsplash to do one thing and do it well

--- fish/functions/unsplash.fish
+++ fish/functions/unsplash.fish
@@ -3,21 +3,10 @@ function unsplash
     set --local UNSPLASH_ACCESS_KEY (op get item unsplash | jq --raw-output '.details.sections[1].fields[0].v')
 
     set --local PHOTO_ID $argv[1]
-    set --local RANDOM_NUMBER (random)
-
-    set --local DROPBOX_IMAGES ~/Dropbox/Uploads/todays_picture_*.jpg
-    set --local DROPBOX_WALLPAPER ~/Dropbox/Uploads/todays_picture_$RANDOM_NUMBER.jpg
-
-    set --local BASE_URL "https://api.unsplash.com"
-    set --local API_IMAGE_URL "$BASE_URL/photos/$PHOTO_ID"
+    set --local API_IMAGE_URL "https://api.unsplash.com/photos/$PHOTO_ID"
     set --local UNSPLASH_RESPONSE (http --body $API_IMAGE_URL "Authorization: Client-ID $UNSPLASH_ACCESS_KEY")
     set --local IMAGE_URL (echo $UNSPLASH_RESPONSE | jq --raw-output ".urls.full")
+    set --local DOWNLOADED_IMAGE ~/Downloads/todays_picture_$PHOTO_ID.jpg
 
-    ##################################################################################################
-    # Downloading to Dropbox so that I can manually set my iPhone wallpaper photo from there
-    ##################################################################################################
-    rm -f $DROPBOX_IMAGES
-    wget --quiet --output-document=$DROPBOX_WALLPAPER $IMAGE_URL
-    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$DROPBOX_WALLPAPER\""
-
+    wget --quiet --output-document=$DOWNLOADED_IMAGE $IMAGE_URL
 end
