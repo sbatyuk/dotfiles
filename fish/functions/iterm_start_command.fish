e8cb9ec6a6beb18e7e25388ba15408b57d9b56c3
Sam Hosseini <git-email@hosseini.io>
2020-12-22 00:18:13 +0200

    refactor iterm_start_command

--- fish/functions/iterm_start_command.fish
+++ fish/functions/iterm_start_command.fish
@@ -1,18 +1,3 @@
 function iterm_start_command
-    # This command is sent to a new shell when iTerm launches
-
-    clear
-
-    # Kill all background jobs
-    jobs -p | xargs kill
-
-    # Send signals to macOS to prevent sleep
-    caffeinate -d &
-
-    # Load tmux session
-    if tmux new-session -s base -n '    '
-        return
-    else
-        tmux attach-session -t base
-    end
+    caffeinate -d tmux new-session -A -s base -n '    '
 end
