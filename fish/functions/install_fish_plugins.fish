a5a9c8214e3689475f35e10d3172307a32e5ccd6
Sam Hosseini <git-email@hosseini.io>
2020-12-21 19:15:24 +0200

    rename install_fisher to install_fish_plugins

--- fish/functions/install_fish_plugins.fish
+++ fish/functions/install_fish_plugins.fish
@@ -1,4 +1,4 @@
-function install_fisher
+function install_fish_plugins
     set --local FISH_PLUGINS (cat $DOTFILES_REPO/fish/fish_plugins)
 
     curl --silent --location https://git.io/fisher | source
