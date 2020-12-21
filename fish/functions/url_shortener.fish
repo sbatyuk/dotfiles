326cc6bbd9eea11ece6351d01a04c140ec9d1f54
Sam Hosseini <git-email@hosseini.io>
2020-12-21 22:43:45 +0200

    rename shorten to url_shortener

--- fish/functions/url_shortener.fish
+++ fish/functions/url_shortener.fish
@@ -1,4 +1,4 @@
-function shorten
+function url_shortener
     1password_session
     set --local BITLY_API_TOKEN (op get item bitly | jq --raw-output '.details.sections[1].fields[0].v')
 
@@ -7,6 +7,7 @@ function shorten
     set --local LONG_URL $argv[1]
     set --local RESPONSE (http --body POST $API_URL $HEADER long_url=$LONG_URL domain=bit.ly)
     set --local SHORTENED_URL (echo $RESPONSE | jq --raw-output '.id')
+
     echo $SHORTENED_URL | pbcopy
     echo $SHORTENED_URL coppied to clipboard!
 end
