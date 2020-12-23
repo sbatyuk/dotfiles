91da65cb4f46015cc3545e6ae2ac7cede52e4ec8
Sam Hosseini <git-email@hosseini.io>
2020-12-23 23:43:15 +0200

    refactor fish env vars to its own fish module

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,6 @@
-############################################################################
-# Environment Variables
-############################################################################
-set --global --export EDITOR vim
-set --global --export PYTHONDONTWRITEBYTECODE true
-set --global --export PYTHONUNBUFFERED true
-set --global --export fish_greeting ''
-set --global --export LC_ALL en_US.UTF-8 # Set locale
-set --global --export LANG en_US.UTF-8   # Set locale
 set --global --export DOTFILES_REPO ~/repos/dotfiles
-set --global --export BAT_THEME Dracula
 
+source $DOTFILES_REPO/fish/environment.fish
 source $DOTFILES_REPO/fish/path.fish
 source $DOTFILES_REPO/git/contact_info.fish
 source $DOTFILES_REPO/homebrew/environment.fish
---
+++ fish/environment.fish
@@ -0,0 +1,9 @@
+set --global --export LANG   en_US.UTF-8 # Set locale
+set --global --export LC_ALL en_US.UTF-8 # Set locale
+
+set --global --export PYTHONDONTWRITEBYTECODE   true
+set --global --export PYTHONUNBUFFERED          true
+
+set --global --export BAT_THEME         Dracula
+set --global --export EDITOR            vim
+set --global --export fish_greeting     ''
