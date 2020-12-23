bc9bd9a578682d42746475423b46968338dc0a93
Sam Hosseini <git-email@hosseini.io>
2020-12-23 23:43:13 +0200

    refactor homebrew env vars to its own fish module

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,10 @@
 ############################################################################
 # Environment Variables
 ############################################################################
-set --global --export HOMEBREW_BUNDLE_NO_LOCK true
-set --global --export HOMEBREW_NO_AUTO_UPDATE true
 set --global --export EDITOR vim
 set --global --export PYTHONDONTWRITEBYTECODE true
 set --global --export PYTHONUNBUFFERED true
 set --global --export fish_greeting ''
-set --global --export HOMEBREW_CASK_OPTS "--no-quarantine"
 set --global --export LC_ALL en_US.UTF-8 # Set locale
 set --global --export LANG en_US.UTF-8   # Set locale
 set --global --export DOTFILES_REPO ~/repos/dotfiles
@@ -15,4 +12,5 @@ set --global --export BAT_THEME Dracula
 
 source $DOTFILES_REPO/fish/path.fish
 source $DOTFILES_REPO/git/contact_info.fish
+source $DOTFILES_REPO/homebrew/environment.fish
 source $DOTFILES_REPO/starship/initializer.fish
---
+++ homebrew/environment.fish
@@ -0,0 +1,3 @@
+set --global --export HOMEBREW_CASK_OPTS            "--no-quarantine"
+set --global --export HOMEBREW_BUNDLE_NO_LOCK       true
+set --global --export HOMEBREW_NO_AUTO_UPDATE       true
