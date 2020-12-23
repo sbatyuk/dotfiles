4bde21615f02a688e0cd5631801475a864cfcce7
Sam Hosseini <git-email@hosseini.io>
2020-12-23 05:16:13 +0200

    minor flag adjustments to update_homebrew

--- fish/functions/update_homebrew.fish
+++ fish/functions/update_homebrew.fish
@@ -3,7 +3,7 @@ function update_homebrew
 
     brew update | grep --invert-match "Already up-to-date."
     brew upgrade
-    brew outdated --cask --greedy --verbose | grep --invert-match latest | awk '{print $1;}' | xargs brew upgrade --cask
-    brew cleanup
+    brew outdated --greedy --verbose | grep --invert-match latest | awk '{print $1;}' | xargs brew upgrade
+    brew cleanup --quiet
     brew doctor | grep --invert-match "Your system is ready to brew."
 end
