286348579a4643039ef439072fceb5f281b5eaef
Sam Hosseini <git-email@hosseini.io>
2020-12-24 14:55:08 +0200

    refactor fish/environment.fish

--- fish/environment.fish
+++ fish/environment.fish
@@ -1,9 +1,5 @@
-set --global --export LANG   en_US.UTF-8 # Set locale
-set --global --export LC_ALL en_US.UTF-8 # Set locale
-
+set --global --export BAT_THEME                 Dracula
+set --global --export EDITOR                    vim
+set --global --export LC_ALL                    en_US.UTF-8 # Set locale bit.ly/3mNj6Ia
 set --global --export PYTHONDONTWRITEBYTECODE   true
-set --global --export PYTHONUNBUFFERED          true
-
-set --global --export BAT_THEME         Dracula
-set --global --export EDITOR            vim
-set --global --export fish_greeting     ''
+set --global --export fish_greeting             ''
