daeb5a9306613aeb22802fa2cc8d0db4522afd00
Sam Hosseini <git-email@hosseini.io>
2020-12-25 01:08:44 +0200

    rename hammerspoon/hammerspoon_preferences to hammerspoon/preferences

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,6 @@
 require('spoon_manager')
 require('reload_configuration')
-require('hammerspoon_preferences')
+require('preferences')
 require('window_management')
 require('quit_fantastical')
 require('hide_ticktick')
