8c1c889a00fce4f40c3dcd6dd4b2c81451e2cb09
Sam Hosseini <git-email@hosseini.io>
2020-12-24 15:39:05 +0200

    add empty line for aesthetics in git/contact_info.fish

--- git/contact_info.fish
+++ git/contact_info.fish
@@ -3,5 +3,6 @@ set --local GIT_EMAIL "git-email@hosseini.io"
 
 set --global --export GIT_AUTHOR_NAME       $GIT_NAME
 set --global --export GIT_COMMITTER_NAME    $GIT_NAME
+
 set --global --export GIT_AUTHOR_EMAIL      $GIT_EMAIL
 set --global --export GIT_COMMITTER_EMAIL   $GIT_EMAIL
