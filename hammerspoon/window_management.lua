343cb2e609bede7589fc1baf3475c4e6b447d90e
Sam Hosseini <git-email@hosseini.io>
2020-12-25 02:36:20 +0200

    remove unnecessary comment in window_management.lua

--- hammerspoon/window_management.lua
+++ hammerspoon/window_management.lua
@@ -6,8 +6,8 @@ local HYPER_KEY = {'cmd', 'alt', 'ctrl', 'shift'}
 
 spoon.MiroWindowsManager:bindHotkeys({
   left          = {HYPER_KEY, "left"    },
-  down          = {HYPER_KEY, "down"    },
-  up            = {HYPER_KEY, "up"      },
   right         = {HYPER_KEY, "right"   },
-  fullscreen    = {HYPER_KEY, "b"       }, -- Consistent zooming in tmux and macOS
+  up            = {HYPER_KEY, "up"      },
+  down          = {HYPER_KEY, "down"    },
+  fullscreen    = {HYPER_KEY, "b"       },
 })
