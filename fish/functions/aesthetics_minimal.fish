f6909c2361a1f6eaeca2e7befe400ba4b092a654
Sam Hosseini <git-email@hosseini.io>
2020-12-28 23:33:45 +0200

    rename neofetch ascii files based on their fish function names

--- fish/functions/aesthetics_minimal.fish
+++ fish/functions/aesthetics_minimal.fish
@@ -1,6 +1,6 @@
 function aesthetics_minimal
-    set --local SYMBOL_ASCII_MINIMAL $DOTFILES_REPO/neofetch/symbol_ascii_minimal
+    set --local AESTHETICS_MINIMAL_ASCII $DOTFILES_REPO/neofetch/aesthetics_minimal_ascii
     set --local MODULES_TO_DISABLE cols users model distro song term shell cpu gpu memory
 
-    neofetch --ascii $SYMBOL_ASCII_MINIMAL --disable $MODULES_TO_DISABLE
+    neofetch --ascii $AESTHETICS_MINIMAL_ASCII --disable $MODULES_TO_DISABLE
 end
