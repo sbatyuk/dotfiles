1a34aa41bd683e4233887fb12e7e12c36f76e48e
Sam Hosseini <git-email@hosseini.io>
2020-12-31 13:49:23 +0200

    update fish related paths in config.fish

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,7 +1,7 @@
 set --global --export DOTFILES_REPO ~/repos/dotfiles
 
-source $DOTFILES_REPO/fish/environment.fish
-source $DOTFILES_REPO/fish/path.fish
+source $DOTFILES_REPO/roles/fish/files/environment.fish
+source $DOTFILES_REPO/roles/fish/files/path.fish
 source $DOTFILES_REPO/git/contact_info.fish
 source $DOTFILES_REPO/homebrew/environment.fish
 source $DOTFILES_REPO/starship/initializer.fish
