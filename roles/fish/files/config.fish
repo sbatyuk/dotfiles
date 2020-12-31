cc3b728f7ffdb1588a867d207532e19531af5020
Sam Hosseini <git-email@hosseini.io>
2020-12-31 18:12:25 +0200

    move starship files to ansible fish role

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,7 +1,7 @@
 set --global --export DOTFILES_REPO ~/repos/dotfiles
 
+source $DOTFILES_REPO/roles/fish/files/starship.fish
 source $DOTFILES_REPO/roles/fish/files/environment.fish
 source $DOTFILES_REPO/roles/fish/files/path.fish
 source $DOTFILES_REPO/git/contact_info.fish
 source $DOTFILES_REPO/homebrew/environment.fish
-source $DOTFILES_REPO/starship/initializer.fish
