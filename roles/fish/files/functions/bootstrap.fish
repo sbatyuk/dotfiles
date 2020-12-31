83776d8c19efc292ae8227233aedcbd3e2ed9a76
Sam Hosseini <git-email@hosseini.io>
2020-12-31 15:42:52 +0200

    update bootstrap fish function to accept cli args

--- roles/fish/files/functions/bootstrap.fish
+++ roles/fish/files/functions/bootstrap.fish
@@ -1,4 +1,4 @@
 function bootstrap
     sudo --validate
-    ansible-playbook --inventory $DOTFILES_REPO/hosts.ini $DOTFILES_REPO/bootstrap.yml
+    ansible-playbook --inventory $DOTFILES_REPO/hosts.ini $DOTFILES_REPO/bootstrap.yml $argv
 end
