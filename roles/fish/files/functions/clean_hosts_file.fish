ccb0148c371a784c37277a2f64f4d42cc338f0f3
Sam Hosseini <git-email@hosseini.io>
2021-01-18 01:33:41 +0200

    update clean_hosts_file and remove update_hosts_file fish functions

--- roles/fish/files/functions/clean_hosts_file.fish
+++ roles/fish/files/functions/clean_hosts_file.fish
@@ -1,4 +1,4 @@
 function clean_hosts_file
-    sudo cp $DOTFILES_REPO/hosts/base_hosts_file /etc/hosts
+    sudo cp $DOTFILES_REPO/roles/hosts/files/base_hosts_file /private/etc/hosts
     less /etc/hosts
 end
--- roles/fish/files/functions/update_hosts_file.fish
+++
@@ -1,7 +0,0 @@
-function update_hosts_file
-    bash -c '
-    source $DOTFILES_REPO/bootstrap.sh --source-only
-    update_hosts_file
-    '
-    less /etc/hosts
-end
