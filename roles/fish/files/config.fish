c91c6005507bf97b4e98917886690d8bade1b812
Sam Hosseini <git-email@hosseini.io>
2021-01-17 20:17:10 +0200

    add git name and email directly to git config file

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -3,5 +3,4 @@ set --global --export DOTFILES_REPO ~/repos/dotfiles
 source $DOTFILES_REPO/roles/fish/files/starship.fish
 source $DOTFILES_REPO/roles/fish/files/environment.fish
 source $DOTFILES_REPO/roles/fish/files/path.fish
-source $DOTFILES_REPO/roles/git/files/contact_info.fish
 source $DOTFILES_REPO/homebrew/environment.fish
--- roles/git/files/contact_info.fish
+++
@@ -1,8 +0,0 @@
-set --local GIT_NAME  "Sam Hosseini"
-set --local GIT_EMAIL "git-email@hosseini.io"
-
-set --global --export GIT_AUTHOR_NAME       $GIT_NAME
-set --global --export GIT_COMMITTER_NAME    $GIT_NAME
-
-set --global --export GIT_AUTHOR_EMAIL      $GIT_EMAIL
-set --global --export GIT_COMMITTER_EMAIL   $GIT_EMAIL
