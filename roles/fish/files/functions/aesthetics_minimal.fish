b4c9745b090e7732ebb2cab5e35921c0d07fca9a
Sam Hosseini <git-email@hosseini.io>
2021-01-18 12:44:55 +0200

    update neofetch ascii files path in its config and fish function

--- roles/fish/files/functions/aesthetics_minimal.fish
+++ roles/fish/files/functions/aesthetics_minimal.fish
@@ -1,5 +1,5 @@
 function aesthetics_minimal
-    set --local AESTHETICS_MINIMAL_ASCII $DOTFILES_REPO/neofetch/aesthetics_minimal_ascii
+    set --local AESTHETICS_MINIMAL_ASCII $DOTFILES_REPO/roles/neofetch/files/aesthetics_minimal_ascii
     set --local MODULES_TO_DISABLE cols users model distro song term shell cpu gpu memory
 
     neofetch --ascii $AESTHETICS_MINIMAL_ASCII --disable $MODULES_TO_DISABLE
