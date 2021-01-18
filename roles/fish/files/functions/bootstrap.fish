974837db597c0bd180aab75874a61d6a3d9520e4
Sam Hosseini <git-email@hosseini.io>
2021-01-18 02:43:50 +0200

    add inventory path to ansible.cfg

--- roles/fish/files/functions/bootstrap.fish
+++ roles/fish/files/functions/bootstrap.fish
@@ -1,4 +1,4 @@
 function bootstrap --wraps "ansible-playbook"
     sudo --validate
-    ansible-playbook --inventory $DOTFILES_REPO/hosts.ini $DOTFILES_REPO/bootstrap.yml $argv
+    ansible-playbook $DOTFILES_REPO/bootstrap.yml $argv
 end
