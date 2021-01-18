76cdbec8fd04d7dfc72de17aaa8f6a7c38f0e9fc
Sam Hosseini <git-email@hosseini.io>
2021-01-18 16:36:25 +0200

    add pipx to dotfiles

--- roles/fish/files/path.fish
+++ roles/fish/files/path.fish
@@ -1,5 +1,6 @@
 set --local --append OWN_PATH /usr/local/bin                        # Where brew symlinks most packages
 set --local --append OWN_PATH /usr/local/sbin                       # Where brew symlinks some packages
 set --local --append OWN_PATH /usr/local/opt/ruby/bin               # Where brew's ruby lives
+set --local --append OWN_PATH ~/.local/bin                          # Where pipx symlinks its packages
 
 set --global --export fish_user_paths $OWN_PATH
