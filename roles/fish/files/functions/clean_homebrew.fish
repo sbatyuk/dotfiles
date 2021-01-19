ec99897ecda85b376db0ba532f2149b01f8c9199
Sam Hosseini <git-email@hosseini.io>
2021-01-19 03:02:00 +0200

    update homebrew files path in clean_homebrew fish function

--- roles/fish/files/functions/clean_homebrew.fish
+++ roles/fish/files/functions/clean_homebrew.fish
@@ -1,8 +1,8 @@
 function clean_homebrew
-    set --local TAP  $DOTFILES_REPO/homebrew/tap
-    set --local BREW $DOTFILES_REPO/homebrew/brew
-    set --local CASK $DOTFILES_REPO/homebrew/cask
-    set --local MAS  $DOTFILES_REPO/homebrew/mas
+    set --local TAP  $DOTFILES_REPO/roles/homebrew/files/tap
+    set --local BREW $DOTFILES_REPO/roles/homebrew/files/brew
+    set --local CASK $DOTFILES_REPO/roles/homebrew/files/cask
+    set --local MAS  $DOTFILES_REPO/roles/homebrew/files/mas
 
     cat $TAP $BREW $CASK $MAS | brew bundle cleanup --force --file=-
 end
