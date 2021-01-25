9b0ea4dca818b1c4af59bcf818b96eb458defe59
Sam Hosseini <git-email@hosseini.io>
2021-01-26 00:34:03 +0200

    delete update_login_items from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,14 +12,3 @@ function setup_macOS_defaults() {
         exit 1
     fi
 }
-
-function update_login_items() {
-    info "Updating login items"
-
-    if osascript ${DOTFILES_REPO}/macOS/login_items.applescript &> /dev/null; then
-        success "Login items updated successfully "
-    else
-        error "Login items update failed"
-        exit 1
-    fi
-}
