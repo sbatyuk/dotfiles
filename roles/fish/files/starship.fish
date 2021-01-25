0ce457885f9b178faa3df4254433bee4441259b7
Sam Hosseini <git-email@hosseini.io>
2021-01-25 21:57:36 +0200

    refactor starship in fish ansible role

--- roles/fish/files/starship.fish
+++ roles/fish/files/starship.fish
@@ -1,3 +1,3 @@
-set --global --export STARSHIP_CONFIG   ~/.config/starship/starship.toml
+set --global --export STARSHIP_CONFIG           $DOTFILES_REPO/roles/fish/files/starship.toml
 
 starship init fish | source
