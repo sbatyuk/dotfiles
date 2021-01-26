62b23bcabf837c36f2ea89e3cdbe9cef9b297cbd
Sam Hosseini <git-email@hosseini.io>
2021-01-26 18:21:09 +0200

    move dock preferences to ansible macos role

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -1,32 +1,7 @@
 #!/usr/bin/env bash
 
 main() {
-    configure_dock
     configure_finder
-}
-
-function configure_dock() {
-    quit "Dock"
-    # Don’t show recent applications in Dock
-    defaults write com.apple.dock show-recents -bool false
-    # Set the icon size of Dock items to 36 pixels
-    defaults write com.apple.dock tilesize -int 36
-    # Remove all (default) app icons from the Dock
-    defaults write com.apple.dock persistent-apps -array
-    defaults write com.apple.dock recent-apps -array
-    # Show only open applications in the Dock
-    defaults write com.apple.dock static-only -bool true
-    # Don’t animate opening applications from the Dock
-    defaults write com.apple.dock launchanim -bool false
-    # Automatically hide and show the Dock
-    defaults write com.apple.dock autohide -bool true
-    # Remove the auto-hiding Dock delay
-    defaults write com.apple.dock autohide-delay -float 0
-    # Disable the Launchpad gesture (pinch with thumb and three fingers)
-    defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
-    # Disable automatically rearranging of Spaces based on most recent use
-    defaults write com.apple.dock mru-spaces -bool false
-    open "Dock"
 }
 
 function configure_finder() {
@@ -71,21 +46,6 @@ function configure_finder() {
     defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
     # Disable screenshot preview thumbnails
     defaults write com.apple.screencapture show-thumbnail -bool false
-}
-
-function quit() {
-    app=$1
-    killall "$app" > /dev/null 2>&1
-}
-
-function open() {
-    app=$1
-    osascript << EOM
-tell application "$app" to activate
-tell application "System Events" to tell process "iTerm2"
-set frontmost to true
-end tell
-EOM
 }
 
 main "$@"
