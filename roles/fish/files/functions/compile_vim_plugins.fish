029753dbcbd52999fc1350da4965a1f9ec421ece
Sam Hosseini <git-email@hosseini.io>
2021-01-27 22:32:34 +0200

    remove YouCompleteMe from vim as vscode has it already

--- roles/fish/files/functions/compile_vim_plugins.fish
+++ roles/fish/files/functions/compile_vim_plugins.fish
@@ -1,9 +1,6 @@
 function compile_vim_plugins
     set --local CURRENT_DIR (pwd)
 
-    # Compiling the compiled component of YouCompleteMe
-    python3 ~/.vim/bundle/YouCompleteMe/install.py
-
     # Compiling the compiled component of Command-t
     cd ~/.vim/bundle/command-t
     rake make
