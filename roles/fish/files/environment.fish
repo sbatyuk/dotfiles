c398162b71010a0c343b9ec93ff2d6686d91872d
Sam Hosseini <git-email@hosseini.io>
2021-01-30 14:56:19 +0200

    update EDITOR to be nvim by default

--- roles/fish/files/environment.fish
+++ roles/fish/files/environment.fish
@@ -1,6 +1,6 @@
 set --global --export ANSIBLE_CONFIG            $DOTFILES_REPO/ansible/ansible.cfg
 set --global --export BAT_THEME                 Dracula
-set --global --export EDITOR                    vim
+set --global --export EDITOR                    nvim
 set --global --export LC_ALL                    en_US.UTF-8 # Set locale bit.ly/3mNj6Ia
 set --global --export PYTHONDONTWRITEBYTECODE   true
 set --global --export fish_greeting             ''
