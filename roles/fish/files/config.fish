bf769fe2f00a19b2333bca08e3184fd468599a30
Sam Hosseini <git-email@hosseini.io>
2021-02-03 01:02:29 +0200

    add fish config template to fish role

--- roles/fish/files/config.fish
+++
@@ -1,6 +0,0 @@
-set --global --export DOTFILES_REPO /Users/sam_hosseini/repos/dotfiles
-
-source $DOTFILES_REPO/roles/fish/files/starship.fish
-source $DOTFILES_REPO/roles/fish/files/environment.fish
-source $DOTFILES_REPO/roles/fish/files/path.fish
-source $DOTFILES_REPO/roles/homebrew/files/environment.fish
