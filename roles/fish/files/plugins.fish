1edb5a098bbd4dbf194e63b2e68c934a8892a0b2
Sam Hosseini <git-email@hosseini.io>
2021-02-02 23:49:39 +0200

    remove comments from plugins.fish

--- roles/fish/files/plugins.fish
+++ roles/fish/files/plugins.fish
@@ -1,5 +1,4 @@
-jorgebucaran/fisher                 # Plugin manager itself
-jethrokuan/z                        # Pure-fish z directory jumping
-dracula/fish                        # Dracula theme which influences the colors of what you type
-laughedelic/pisces                  # Plugin to help you work with paired symbols
-halostatue/fish-docker              # Docker and docker-compose completions
+jethrokuan/z
+dracula/fish
+laughedelic/pisces
+halostatue/fish-docker
