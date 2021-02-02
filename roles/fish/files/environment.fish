db9c3beef326be4162745d6d466e21a0092bfbf8
Sam Hosseini <git-email@hosseini.io>
2021-02-03 01:26:03 +0200

    move ansible related files to ansible role

--- roles/fish/files/environment.fish
+++ roles/fish/files/environment.fish
@@ -1,4 +1,4 @@
-set --global --export ANSIBLE_CONFIG            $DOTFILES_REPO/ansible/ansible.cfg
+set --global --export ANSIBLE_CONFIG            $DOTFILES_REPO/roles/ansible/files/ansible.cfg
 set --global --export BAT_THEME                 Dracula
 set --global --export EDITOR                    nvim
 set --global --export LC_ALL                    en_US.UTF-8 # Set locale bit.ly/3mNj6Ia
--- roles/fish/files/functions/bootstrap.fish
+++ roles/fish/files/functions/bootstrap.fish
@@ -1,4 +1,4 @@
 function bootstrap --wraps "ansible-playbook"
     sudo --validate
-    ansible-playbook $DOTFILES_REPO/ansible/bootstrap.yml $argv
+    ansible-playbook $DOTFILES_REPO/roles/ansible/files/bootstrap.yml $argv
 end
